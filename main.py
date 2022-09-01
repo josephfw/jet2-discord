cc274a85f74abbab67ce3ebf1fa1a5f84fa0ab27
Joseph <joeeeeee06@gmail.com>
2022-09-01 21:55:51 +0100

    Environment update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from interactions.ext.wait_for import setup
 
 dotenv.load_dotenv()
 
-CLIENT = interactions.Client(token=os.environ.get('jet2botkey'), intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
+CLIENT = interactions.Client(token=os.environ.get('key'), intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
 
 CLIENT.load("commands.uptime")
 CLIENT.load("commands.announce")
